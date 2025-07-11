5717dc525a554561f73d4947db0e65f11eaf67d8
Web-Dev46 <kabilr927@gmail.com>
2025-07-11 19:39:44 +0530

    first commit

---
+++ product.js
@@ -0,0 +1,12 @@
+
+if (document.querySelector(".swiper")) {
+  new Swiper(".swiper", {
+    loop: true,
+    autoplay: { delay: 3000 },
+    pagination: { el: ".swiper-pagination", clickable: true },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
